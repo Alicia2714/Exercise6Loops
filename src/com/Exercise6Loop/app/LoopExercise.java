fb2002345abfe865efe63f5763cc7f5dee340f93
Alicia2714 <Alicia_Sanchez2714@hotmail.com>
2019-10-14 13:09:10 -0500

    Finished examples of loops

--- src/com/Exercise6Loop/app/LoopExercise.java
+++ src/com/Exercise6Loop/app/LoopExercise.java
@@ -1,10 +1,51 @@
 package com.Exercise6Loop.app;
 
+
+
 public class LoopExercise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Constant declaration
+		
+		//Variable declaration
+		
+		//Object declaration
+		
+		//Create a series of 1 to 100
+		for (int i=0; i<=100; i++) {
+			System.out.println(i);
+			
+		}
+		
+		//Creates a series of 100 to 1
+		for (int i=100; i>=1; i--) {
+			System.out.println(1);
+		}
+		
+		//Creates a series in pair from 0 to 100
+		for (int i=0; i<=100; i+=2) {
+			System.out.println(i);
+		}
+		
+		//Creates a series in pairs from 100 to 200
+		for (int i=100; i<200; i++) {
+			if(i%2==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		System.out.println("Displays half of the series ");
+		
+		for (int i=100; i<200; i++) {
+			System.out.println(i);
+			if(i==150) {
+				break;
+			}
+		}
+		
+		
 	}
 
 }
